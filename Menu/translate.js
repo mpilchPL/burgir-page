b1387d32bea882a81e9c6687b840e093dbb930f9
MichalKaim <kontakt.michalkaim@gmail.com>
2021-11-04 10:09:58 +0100

    Working on translating script

---
+++ Menu/translate.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", ChangeLanguage)
+lang = "en"
+
+function ChangeLanguage(){
+    var text = document.getElementsByClassName("text");
+    
+    var englishDictionary = [
+        "Main Page",
+        "About Us",
+        "Menu",
+        "Contact",
+        "Book table",
+        "No.",
+        "Name",
+        "Ingredients",
+        "Prince (PLN)"
+    ]
+    
+    var polishDictionary = [
+        "Strona Główna",
+        "O nas",
+        "Menu",
+        "Kontakt",
+        "Zarezerwuj",
+        "Nr.",
+        "Nazwa",
+        "Składniki",
+        
+    ]
+    
+    for(let i = 0; i < text.length; i++){
+        if(lang == "en"){
+            text[i].innerHTML = englishDictionary[i]
+        }
+        else if(lang == "pl"){
+
+        }
+        
+    }
+}
