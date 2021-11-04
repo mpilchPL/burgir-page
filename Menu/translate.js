e0376e72e8afdbd751634de9b14d3b04d8de442e
MichalKaim <kontakt.michalkaim@gmail.com>
2021-11-04 10:37:58 +0100

    Finish Translate Script

--- Menu/translate.js
+++ Menu/translate.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", ChangeLanguage)
-lang = "en"
+
+function PolishFlagClicked(){
+    localStorage.setItem("language", "pl");
+    ChangeLanguage();
+}
+
+function EnglishFlagClicked(){
+    localStorage.setItem("language", "en");
+    ChangeLanguage();
+}
 
 function ChangeLanguage(){
     var text = document.getElementsByClassName("text");
@@ -14,6 +23,39 @@ function ChangeLanguage(){
         "Name",
         "Ingredients",
         "Prince (PLN)"
+    ]
+
+    var englishIngredients =[
+        "Cheese, Tomato sauce",
+        "Cheese, Tomato sauce, Salame, Mushrooms",
+        "Cheese, Tomato sauce, Mushrooms",
+        "Cheese, Tomato sauce, Salame, Jalapeno",
+        "Cheese, Tomato sauce, Chicken, Paprika, Mushrooms",
+        "Cheese, Tomato sauce, Ham, Pineapple",
+        "Nutella, Banana",
+        "Frozen cheese, Frozen tomato sauce, Frozen ham",
+        "Cheese, Tomato sauce, Ham, Mozarella",
+        "Cheese, Tomato sauce, Olives, Feta cheese, Rucola",
+        "Cheese, Tomato sauce, Salame, Jalapeno",
+        "Cheese, Tomato sauce, Chicken, Paprika, Mushrooms",
+        "Cheese, Tomato sauce, Ham, Chicken, Salame, Mozarella",
+
+    ]
+
+    var polishIngredients =[
+        "Ser, Sos pomidorowy",
+        "Ser, Sos pomidorowy, Salami, Pieczarki",
+        "Ser, Sos pomidorowy, Pieczarki",
+        "Ser, Sos pomidorowy, Salami, Jalapeno",
+        "Ser, Sos pomidorowy, Kurczak, Papryka, Pieczarki",
+        "Ser, Sos pomidorowy, Szynka, Ananas",
+        "Nutella, Banan",
+        "Mrożony Ser, Mrożony Sos pomidorowy, Mrożony Szynka",
+        "Ser, Sos pomidorowy, Szynka, Mozarella",
+        "Ser, Sos pomidorowy, Salami, Jalapeno",
+        "Ser, Sos pomidorowy, Kurczak, Papryka, Pieczarki",
+        "Ser, Sos pomidorowy, Oliwki, Ser feta, Rukola",
+        "Ser, Sos pomidorowy, Szynka, Kurczak, Salami, Mozarella",
     ]
     
     var polishDictionary = [
@@ -30,11 +72,23 @@ function ChangeLanguage(){
     ]
     
     for(let i = 0; i < text.length; i++){
-        if(lang == "en"){
+        if(localStorage.getItem("language") == "en"){
             text[i].innerHTML = englishDictionary[i]
         }
-        else if(lang == "pl"){
+        else{
             text[i].innerHTML = polishDictionary[i]
+        }
+        
+    }
+
+    var ingredients = document.getElementsByClassName("ingredients");
+
+    for(let i = 0; i < ingredients.length; i++){
+        if(localStorage.getItem("language") == "en"){
+            ingredients[i].innerHTML = englishIngredients[i]
+        }
+        else{
+            ingredients[i].innerHTML = polishIngredients[i]
         }
         
     }
